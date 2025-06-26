959943d649774ffb44fe2ad378360e290a2a5d75
EdielD7 <ediel.purim@hotmail.com>
2025-06-26 01:55:28 -0400

    atualiza Header da pagina Profile

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,49 @@
-import { HeaderBar, Titulo } from './styles'
-import logo from '../../assets/images/logo.svg'
-import { Link } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 
-const Header = () => (
-  <HeaderBar>
-    <Link to="/">
-      <img src={logo} alt="efood" />
-    </Link>
-    <Titulo>
-      Viva experiências gastronômicas <br /> no conforto da sua casa
-    </Titulo>
-  </HeaderBar>
-)
+import {
+  HeaderBar,
+  Links,
+  CartLink,
+  Titulo,
+  RestaurantesLink,
+  ProfileLogo,
+  HomeLogo
+} from './styles' // Ajustaremos os styles em breve
+
+import logo from '../../assets/images/logo.svg'
+
+const Header = () => {
+  const location = useLocation() // Hook para obter a localização atual
+  const isProfilePage = location.pathname.startsWith('/profile') // Verifica se a rota começa com /profile/
+
+  return (
+    // Passamos a prop 'isProfilePage' para o nosso container de estilo
+    <HeaderBar isProfilePage={isProfilePage}>
+      <div className="container">
+        {/* Renderização condicional dos elementos do Header */}
+        {isProfilePage ? (
+          <>
+            <Links>
+              <li>
+                <RestaurantesLink to="/">Restaurantes</RestaurantesLink>
+              </li>
+            </Links>
+            <ProfileLogo src={logo} alt="EFOOD" />
+            <CartLink>0 produto(s) no carrinho</CartLink>
+          </>
+        ) : (
+          // Conteúdo original da Home Page
+          <>
+            <HomeLogo src={logo} alt="EFOOD" />
+            <Titulo>
+              Viva experiências gastronômicas
+              <br /> no conforto da sua casa
+            </Titulo>
+          </>
+        )}
+      </div>
+    </HeaderBar>
+  )
+}
 
 export default Header
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,20 +1,68 @@
 import styled from 'styled-components'
 import { cores } from '../../styles'
 import headerbg from '../../assets/images/header_bg.svg'
+import { Link } from 'react-router-dom' // A importação é usada aqui
 
-export const HeaderBar = styled.header`
+type HeaderBarProps = {
+  isProfilePage: boolean
+}
+
+export const HeaderBar = styled.header<HeaderBarProps>`
   width: 100%;
   background-color: ${cores.rosa};
   background-image: url(${headerbg});
   padding: 40px;
-  display: flex;
-  flex-direction: column;
-  gap: 138px;
-  align-items: center;
+
+  height: ${(props) => (props.isProfilePage ? '138px' : '360px')};
+
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    flex-direction: ${(props) => (props.isProfilePage ? 'row' : 'column')};
+    position: relative;
+    height: 100%;
+  }
+`
+
+const LogoBase = styled.img`
+  height: 58px;
+`
+
+// Logo para a Home (sem posicionamento especial)
+export const HomeLogo = styled(LogoBase)``
+
+// Logo para a página de Perfil (com posicionamento absoluto)
+export const ProfileLogo = styled(LogoBase)`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 `
 
 export const Titulo = styled.h2`
   font-size: 36px;
   font-weight: 900;
   text-align: center;
+  margin-top: 138px;
+`
+
+export const RestaurantesLink = styled(Link)`
+  text-decoration: none;
+  font-weight: 900;
+  color: ${cores.rosaEscuro};
+  font-size: 18px;
+`
+
+export const Links = styled.ul`
+  display: flex;
+  list-style: none;
+`
+
+export const CartLink = styled.a`
+  display: flex;
+  cursor: pointer;
+  font-weight: 900;
+  font-size: 18px;
 `
